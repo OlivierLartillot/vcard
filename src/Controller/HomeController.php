ec0b927093a4081e05cce6d52042a7453b34e0fa
Olivier Lartillot <olivier.lartillot@gmail.com>
2023-03-21 11:30:33 +0100

    tentative de template

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use JeroenDesloovere\VCard\VCard;
+
+class HomeController extends AbstractController
+{
+
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+
+    #[Route('/test', name: 'app_test')]
+    public function test(): Response
+    {
+
+
+        // define vcard
+        $vcard = new VCard();
+        
+        // define variables
+        $lastname = 'Desloovere';
+        $firstname = 'Jeroen';
+        $additional = '';
+        $prefix = '';
+        $suffix = '';
+    
+
+
+        // add personal data
+        $vcard->addName($lastname, $firstname, $additional, $prefix, $suffix);
+    
+        
+        // add work data
+        $vcard->addCompany('Siesqo');
+        $vcard->addJobtitle('Web Developer');
+        $vcard->addRole('Data Protection Officer');
+        $vcard->addEmail('info@jeroendesloovere.be');
+        $vcard->addPhoneNumber(1234121212, 'PREF;WORK');
+        $vcard->addPhoneNumber(123456789, 'WORK');
+        $vcard->addAddress(null, null, 'street', 'worktown', null, 'workpostcode', 'Belgium');
+        $vcard->addLabel('street, worktown, workpostcode Belgium');
+        $vcard->addURL('http://www.jeroendesloovere.be');
+        
+        
+       // $vcard->addPhoto(__DIR__ . '/landscape.jpeg');
+       
+       // return vcard as a string
+       // return $vcard->getOutput();
+       // $vcard->getOutput();
+       // return vcard as a download
+       // return $vcard->download();
+        $download = $vcard->download();
+       
+       // save vcard on disk
+       //$vcard->setSavePath('/path/to/directory');
+       //$vcard->save();
+
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+            'download' => $download
+        ]);
+    }
+}
